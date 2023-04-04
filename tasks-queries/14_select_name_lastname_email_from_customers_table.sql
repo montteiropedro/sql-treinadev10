b7ab4c02414435c8cdc49e25be1ad84432bdcaa9
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-04 19:56:53 -0300

    changes: update comments in task 14

--- tasks-queries/14_select_name_lastname_email_from_customers_table.sql
+++ tasks-queries/14_select_name_lastname_email_from_customers_table.sql
@@ -1,5 +1,5 @@
 -- 14. Construa uma consulta capaz de exibir somente o name, lastname e email dos clientes que moram no estado de SP.
 
--- Run this query to select name, lastname and email from all users who live in the state of São Paulo from the CUSTOMERS table.
+-- Run this query to select, from the CUSTOMERS table, the name, lastname and email from all users who live in the state of São Paulo.
 SELECT NAME, LASTNAME, EMAIL FROM CUSTOMERS
   WHERE STATE = "SP";
