bfa1fc96728708bac9c8d2e80ea383a7afb806ba
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-04 20:00:58 -0300

    add: task 16 - select cars from 2016 from CARS table

---
+++ tasks-queries/16_select_cars_from_2016_from_cars_table.sql
@@ -0,0 +1,8 @@
+-- 16. Construa uma consulta capaz de exibir todos os automóveis do ano 2016.
+
+-- Run this query to see all cars from CARS table.
+SELECT * FROM CARS;
+
+-- Run this query to see cars that are from 2016.
+SELECT * FROM CARS
+  WHERE YEAR = 2016;
