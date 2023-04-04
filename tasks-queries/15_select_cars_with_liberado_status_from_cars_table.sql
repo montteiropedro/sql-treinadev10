f32a009e8547c711a922ba4014d96f6d40b3f9ef
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-04 19:55:57 -0300

    add: task 15 - select all cars with status of Liberado from CARS table

---
+++ tasks-queries/15_select_cars_with_liberado_status_from_cars_table.sql
@@ -0,0 +1,5 @@
+-- 15. Construa uma consulta capaz de exibir somente os automóveis que estão com o status “Liberado”.
+
+-- Run this query to select, from CARS table, all cars with status of "Liberado".
+SELECT * FROM CARS
+  WHERE STATUS = "Liberado";
