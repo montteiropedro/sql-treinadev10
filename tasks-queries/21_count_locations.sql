b31ee5c53cb70325abce30d5bc3e481902909f1a
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-04 21:04:33 -0300

    add: task 21 - count how many locations exist in the LOCATIONS table

---
+++ tasks-queries/21_count_locations.sql
@@ -0,0 +1,4 @@
+-- 21. Construa uma consulta capaz de exibir quantas locações existem na tabela de LOCATIONS.
+
+-- Run this query to select a count of how many locations exist in the LOCATIONS table.
+SELECT COUNT(*) AS TOTAL_LOCATIONS FROM LOCATIONS;
